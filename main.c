e6c6ccf30c0328bb84e9f49e75b33b59fb497c2f
Roberto Sousa <robert6257@gmail.com>
2022-06-10 19:35:47 -0300

    Desclarando variaveis e os prototipos das funçoes

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXIMO 10
+
+//Variaveis
+int tamanho = 0;
+int grafo[MAXIMO];
+int ma[MAXIMO][MAXIMO];
+int op = 1;
+
+//Prototipacao
+int grafo_tamanho();
+void grafo_desenhar();
+void grafo_desenhar_ma();
+void grafo_inserir();
+void grafo_remover();
+void menu_mostrar();
+
+
 int main()
 {
     printf("Hello world!\n");
